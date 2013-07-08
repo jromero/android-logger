da316b6f5313978b95ecfe9bff6a58f8c8c85a32
Javier Romero <javier.romero@bottlerocketapps.com>
2013-07-08 15:49:45 -0500

    Implimented dynamic tag feature

--- AndroidLoggerSample/src/com/noveogroup/android/sample/log/MainActivity.java
+++ AndroidLoggerSample/src/com/noveogroup/android/sample/log/MainActivity.java
@@ -3,10 +3,15 @@ package com.noveogroup.android.sample.log;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+
 import com.noveogroup.android.log.LOG;
+import com.noveogroup.android.log.Logger;
+import com.noveogroup.android.log.LoggerManager;
 import com.noveogroup.android.sample.log.downloader.Downloader;
 
 public class MainActivity extends Activity {
+
+    private static final Logger LOGGER = LoggerManager.getLogger(MainActivity.class, "@@@@@@");
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,4 +28,12 @@ public class MainActivity extends Activity {
         Downloader.download();
     }
 
+    public void onSampleDynamic(View view) {
+        LOGGER.i("This message's tag is not from the configuration");
+    }
+
+    public void onSampleDynamicClass(View view) {
+        LoggerManager.getLogger(MainActivity.class).i(
+                "This is the class name dynamically configured (SimpleName or CanonicalName)");
+    }
 }
